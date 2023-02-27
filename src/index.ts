2f621d0e1686774c6aac66f0a2d7432f08ebc740
JacquelynSH <elpatchaembroidery@gmail.com>
2023-02-27 13:27:56 -0800

    Set up get functions with specified types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import { v4 as uuidV4 } from 'uuid';
+// import { v4 as uuidV4 } from 'uuid';
 
-console.log(uuidV4());
+// console.log(uuidV4());
+
+// use <HTMLUlistelement> to speificy the element type
+const list = document.querySelector<HTMLUListElement>("#list");
+// If you want to get the element by ID you cannot use tthe above syntax - you must use the below
+// it tells you it will receive a form element 
+const form = document.getElementById("#task-form") as HTMLFormElement;
+const input = document.querySelector<HTMLInputElement>("#task-input");
+
