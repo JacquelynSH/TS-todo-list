5d6f269c60e5692a21f38e52e203928680d8c147
JacquelynSH <elpatchaembroidery@gmail.com>
2023-02-27 13:43:50 -0800

    Add event listener that will receive the form input/ create task object to store value

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-// import { v4 as uuidV4 } from 'uuid';
+import { v4 as uuidV4 } from 'uuid';
 
-// console.log(uuidV4());
+console.log(uuidV4());
 
 // use <HTMLUlistelement> to speificy the element type
 const list = document.querySelector<HTMLUListElement>("#list");
@@ -9,3 +9,20 @@ const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.getElementById("#task-form") as HTMLFormElement;
 const input = document.querySelector<HTMLInputElement>("#task-input");
 
+// event listener for form - take in the event object and prevent refresh.
+form?.addEventListener("submit", e => {
+  e.preventDefault();
+// optional chaining (add the ? after the input) - if this exists give the value , if it doesn't return undefined
+// below line ensures that our input exists because it cannot be null.
+  if (input?.value == '' || input?.value == null) return;
+
+  // create a task object that stores the id, title, complete boolean, and when created.
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+
+  input.value
+})
